5af1ec6fd1d4686874945a3a5293fa1b30eb9d54
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-04-19 11:09:44 +0200

    Added a Homescreen

--- whereilikemycoffee/lib/router/routers.js
+++ whereilikemycoffee/lib/router/routers.js
@@ -3,28 +3,5 @@ Router.route('/', function () {
 });
 
 Router.route('/nearby-places', function () {
-	this.render('main');
+	this.render('nearbyPlaces');
 });
-// Router.route('/nearby-places/all', function () {
-// 	this.render('main');
-// });
-
-// Router.route('/nearby-places/busy', function () {
-// 	this.render('main');
-
-// 	Template.placeList.helpers({
-// 		place: function() {
-// 			return Places.find({checkedIn: true}).fetch();
-// 		}
-// 	});
-// });
-
-// Router.route('/nearby-places/quiet', function () {
-// 	this.render('main');
-
-// 	Template.placeList.helpers({
-// 		place: function() {
-// 			return Places.find({checkedIn: false}).fetch();
-// 		}
-// 	});
-// });
